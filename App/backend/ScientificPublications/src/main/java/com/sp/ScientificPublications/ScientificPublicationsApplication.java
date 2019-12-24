9a2695557e444829ea39d80b27335872d67bd988
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2019-12-24 19:39:26 +0100

    Initialized spring boot application

---
+++ App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/ScientificPublicationsApplication.java
@@ -0,0 +1,13 @@
+package com.sp.ScientificPublications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificPublicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificPublicationsApplication.class, args);
+	}
+
+}
---
+++ App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/models/package-info.java
@@ -0,0 +1,1 @@
+package com.sp.ScientificPublications.models;

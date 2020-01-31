a2154f1c39259a3fb512038b7fab00c746ac375a
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2020-01-31 21:19:31 +0100

    Added xml to html transformation method and added used it for cover letter

--- App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/service/DocumentReviewService.java
+++ App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/service/DocumentReviewService.java
@@ -32,7 +32,8 @@ public class DocumentReviewService {
     ExistJaxbRepository existJaxbRepo;
 	
     private static final String schemaPath = "src/main/resources/data/xsd_schema/document-review.xsd";
-    private static final String xslFilePath = "src/main/resources/data/xsl_fo/document-review-fo.xsl";
+    private static final String xslFoFilePath = "src/main/resources/data/xsl_fo/document-review-fo.xsl";
+    private static final String xsltFilePath = "src/main/resources/data/xslt/document-review-xslt.xsl";
     private static final String templatePath = "src/main/resources/templates/document-review-template.xml";
 
     private static final String collectionId = "/db/scientific-publication/document-reviews";
@@ -62,7 +63,7 @@ public class DocumentReviewService {
 
 
     public String generatePdf(String documentId) {
-        return xmlTransformSvc.generatePdfFromXml(retrieveDocumentReviewAsDocument(documentId), xslFilePath);
+        return xmlTransformSvc.generatePdfFromXml(retrieveDocumentReviewAsDocument(documentId), xslFoFilePath);
     }
 
 

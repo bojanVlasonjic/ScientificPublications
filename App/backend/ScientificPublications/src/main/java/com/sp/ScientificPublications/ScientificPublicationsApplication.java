7ba97172ad3ba193525e4e9305145075c29c4722
Milos Malidza <33932996+milosmalidza@users.noreply.github.com>
2020-02-06 23:40:14 +0100

    FIx

--- App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/ScientificPublicationsApplication.java
+++ App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/ScientificPublicationsApplication.java
@@ -1,25 +1,14 @@
 package com.sp.ScientificPublications;
 
-import com.sp.ScientificPublications.repository.exist.ExistUtilityService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScientificPublicationsApplication implements CommandLineRunner {
+public class ScientificPublicationsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScientificPublicationsApplication.class, args);
 	}
 
-	@Autowired
-	private ExistUtilityService existUtilityService;
-
-	@Override
-	public void run(String... args) throws Exception {
-		existUtilityService.getOrCreateCollection("cover-letters");
-		existUtilityService.getOrCreateCollection("paper-reviews");
-		existUtilityService.getOrCreateCollection("scientific-papers");
-	}
 }

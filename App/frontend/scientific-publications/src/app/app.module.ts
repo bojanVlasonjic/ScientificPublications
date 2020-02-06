be28515ec3b2c0850d22b0aedbccf8a1463142fe
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2020-02-06 15:46:25 +0100

    Fixed a conflict issue

--- App/frontend/scientific-publications/src/app/app.module.ts
+++ App/frontend/scientific-publications/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MyScientificPapersComponent,
     EditorComponent,
     ScientificPapersViewComponent,
-    DocumentDownloadComponent
+    DocumentDownloadComponent,
     PendingReviewsComponent
   ],
   imports: [

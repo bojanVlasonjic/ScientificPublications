571b43113546f5530f83ea6f75d1b0a08d3f149a
leonidas97 <ivkovicdjordje1997@gmail.com>
2020-02-06 01:31:25 +0100

    .

--- App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/service/logic/SubmitionService.java
+++ App/backend/ScientificPublications/src/main/java/com/sp/ScientificPublications/service/logic/SubmitionService.java
@@ -10,12 +10,10 @@ import com.sp.ScientificPublications.exception.ApiNotFoundException;
 import com.sp.ScientificPublications.models.Author;
 import com.sp.ScientificPublications.models.Submition;
 import com.sp.ScientificPublications.models.SubmitionStatus;
-import com.sp.ScientificPublications.models.scientific_paper.ScientificPaper;
 import com.sp.ScientificPublications.repository.AuthorRepository;
 import com.sp.ScientificPublications.repository.SubmitionRepository;
 import com.sp.ScientificPublications.service.CoverLetterService;
 import com.sp.ScientificPublications.service.ScientificPaperService;
-import org.checkerframework.checker.nullness.Opt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -266,4 +264,6 @@ public class SubmitionService {
             throw new ApiNotFoundException("Submition and/or reviewer doesnt exist.");
         }
     }
+
+
 }
